7aa740df78187ab65676459efc1e77640705babf
BenderKate <afonia96@gmail.com>
2021-11-08 17:31:40 +0300

    Added Data and Test

--- src/test/java/ru/netology/RegistrationTest.java
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -1,4 +1,25 @@
 package ru.netology;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
 public class RegistrationTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    void shouldLogIn() {
+        RegistrationInfo.RequestData requestData = RegistrationInfo.RequestData.generateUser();
+        $("[data-test-id='login'] [name='login']").setValue(requestData.getLogin());
+        $("[data-test-id='password'] [name='password']").setValue(requestData.getPassword());
+        $("[data-test-id='action-login'] [class='button__text']").click();
+    }
+
 }
